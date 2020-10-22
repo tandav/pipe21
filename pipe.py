ee7770e72f0bdf44d4595f8328821d64ecc7b49e
Alexander Rodionov <alx1dr@gmail.com>
2020-10-22 22:53:08 +0300

    1

--- pipe.py
+++ pipe.py
@@ -3,6 +3,8 @@ import operator
 import subprocess
 import itertools
 import re
+import concurrent.futures
+
 
 # basic
 class B:
@@ -12,8 +14,11 @@ class Map   (B): __ror__ = lambda self, x: map   (self.f, x)
 class Filter(B): __ror__ = lambda self, x: filter(self.f, x)
 
 # extended
-class Reduce(B): __ror__ = lambda self, x: reduce(self.f, x)
+class Reduce        (B): __ror__ = lambda self, x: reduce(self.f, x)
 class MapValues     (B): __ror__ = lambda self, it: it | Map(lambda kv: (kv[0], self.f(kv[1])))
+class MapKeys       (B): __ror__ = lambda self, it: it | Map(lambda kv: (self.f(kv[0]), kv[1]))
+class FilterKeys    (B): __ror__ = lambda self, it: it | Filter(operator.itemgetter(0))
+class FilterValues  (B): __ror__ = lambda self, it: it | Filter(operator.itemgetter(1))
 class FlatMap       (B): __ror__ = lambda self, it: it | Map(self.f) | Pipe(itertools.chain.from_iterable)
 class KeyBy         (B): __ror__ = lambda self, it: it | Map(lambda x: (self.f(x), x))
 class Keys          (B): __ror__ = lambda self, it: it | Map(operator.itemgetter(0))
@@ -26,9 +31,15 @@ class GroupBy       (B): __ror__ = lambda self, it: itertools.groupby(it, key=se
 class ReadLines     (B): __ror__ = lambda self, fn: open(fn).readlines()
 class ShellArg      (B): __ror__ = lambda self, x: subprocess.check_output((self.f, x), text=True).splitlines()
 class ShellExec     (B): __ror__ = lambda self, x: subprocess.check_output(         x , text=True).splitlines()
+class PipeArgs      (B): __ror__ = lambda self, x: self.f(*x)
+class MapArgs       (B): __ror__ = lambda self, x: x | Map(lambda y: y | PipeArgs(self.f))
 class ForEach(B):
     def __ror__(self, x):
         for e in x: self.f(e)
+
+# class 
+
+# class ParallelMap
 
 # class Sorted        (B): __ror__ = lambda self, x: sorted(x, **self.kw)
 
