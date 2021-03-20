ff0ac219b1c9a462983f9225105f121c7cc7deaf
Alexander Rodionov <alx1dr@gmail.com>
2021-03-20 23:44:35 +0300

    upd pipe

--- pipe.py
+++ pipe.py
@@ -5,10 +5,19 @@ import itertools
 import re
 import concurrent.futures
 
+__all__ = [
+    'Pipe', 'Map', 'Filter',
+    'Reduce', 'MapValues', 'MapKeys',
+    'FilterKeys', 'FilterValues', 'FlatMap',
+    'KeyBy', 'ValueBy', 'Keys', 'Values',
+    'Grep', 'GrepV', 'FilterEqual', 'FilterNotEqual',
+    'GroupBy', 'ReadLines', 'ShellArg', 'ShellExec', 'PipeArgs',
+    'MapArgs', 'ForEach', 'ThreadMap', 'ProcessMap', 'count',
+]
 
 # basic
 class B:
-    def __init__(self, f): self.f = f
+    def __init__(self, f=None): self.f = f
 class Pipe  (B): __ror__ = lambda self, x: self.f(x)        
 class Map   (B): __ror__ = lambda self, x: map   (self.f, x)
 class Filter(B): __ror__ = lambda self, x: filter(self.f, x)
@@ -20,7 +29,9 @@ class MapKeys       (B): __ror__ = lambda self, it: it | Map(lambda kv: (self.f(
 class FilterKeys    (B): __ror__ = lambda self, it: it | Filter(operator.itemgetter(0))
 class FilterValues  (B): __ror__ = lambda self, it: it | Filter(operator.itemgetter(1))
 class FlatMap       (B): __ror__ = lambda self, it: it | Map(self.f) | Pipe(itertools.chain.from_iterable)
+# class FlatMapValues (B): __ror__ = lambda self, it: it | Map(self.f) | Pipe(itertools.chain.from_iterable)
 class KeyBy         (B): __ror__ = lambda self, it: it | Map(lambda x: (self.f(x), x))
+class ValueBy       (B): __ror__ = lambda self, it: it | Map(lambda x: (x, self.f(x)))
 class Keys          (B): __ror__ = lambda self, it: it | Map(operator.itemgetter(0))
 class Values        (B): __ror__ = lambda self, it: it | Map(operator.itemgetter(1))
 class Grep          (B): __ror__ = lambda self, it: it | Filter(lambda x:     re.search(self.f, x))
@@ -37,9 +48,20 @@ class ForEach(B):
     def __ror__(self, x):
         for e in x: self.f(e)
 
-# class 
+# flat_map_fn = lambda kv: ((kv[0], x) for x in f(kv[1]))
+#         return self.flatMap(flat_map_fn, preservesPartitioning=True)
 
-# class ParallelMap
+class ThreadMap(B):
+    def __ror__(self, it):
+        with concurrent.futures.ThreadPoolExecutor() as pool:
+            return pool.map(self.f, it) | Pipe(list)
+
+class ProcessMap(B):
+    def __ror__(self, it):
+        with concurrent.futures.ProcessPoolExecutor() as pool:
+            return pool.map(self.f, it) | Pipe(list)
+
+
 
 # class Sorted        (B): __ror__ = lambda self, x: sorted(x, **self.kw)
 
