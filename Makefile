ba0b9882e3f46d2536fb8ad2901dfe5055404199
Alexander Rodionov <alx1dr@gmail.com>
2021-09-04 16:58:07 +0300

    upd for publish to pypi

--- Makefile
+++ Makefile
@@ -1,2 +1,15 @@
 test:
 	python3 -m pytest tests
+
+clean:
+	rm -rf build dist pipe21.egg-info
+
+build: clean
+	python3 setup.py sdist bdist_wheel
+	twine check dist/*
+
+publish: build
+	twine upload dist/*
+
+publish_test: build
+	twine upload --repository-url https://test.pypi.org/legacy/ dist/*
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pipe21',
+    version='1.0.1',
+    description='build functional pipes',
+    long_description_content_type="text/markdown",
+    url='https://github.com/tandav/pipe',
+    packages=find_packages(),
+    include_package_data=True,
+)
