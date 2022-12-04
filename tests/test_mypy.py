e43ca3ea0a3127cf78b16fbcb79deaef0da0b9cb
Alexander Rodionov <tandav@tandav.me>
2022-12-04 18:08:24 +0300

    WIP

---
+++ tests/test_mypy.py
@@ -0,0 +1,12 @@
+import pipe21 as P
+
+
+def bits_to_intervals(bits: str) -> frozenset[int]:
+    return (
+        bits
+        | P.Map(int)
+        | P.Pipe(enumerate)
+        | P.FilterValues()
+        | P.Keys()
+        | P.Pipe(frozenset)
+    )
