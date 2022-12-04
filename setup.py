9a63e4e07b104ec69de432db67f834e7fe4b0fbd
Alexander Rodionov <tandav@tandav.me>
2022-12-04 15:01:50 +0300

    upd infra

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='pipe21',
-    version='1.0.9',
-    description='simple functional pipes',
-    long_description_content_type='text/markdown',
-    url='https://github.com/tandav/pipe21',
-    packages=find_packages(),
-    include_package_data=True,
-)
+setup()
