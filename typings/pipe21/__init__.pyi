d0c7f57ee4194e98abec462126a9e42e08699b02
Alexander Rodionov <tandav@tandav.me>
2023-03-11 20:38:49 +0400

    wip

--- typings/pipe21/__init__.pyi
+++ typings/pipe21/__init__.pyi
@@ -1,5 +1,8 @@
 from collections.abc import Callable
+from typing import Any
 from typing import Generic
+from typing import Iterable
+from typing import Iterator
 from typing import TypeVar
 
 __version__: str = ...
@@ -7,14 +10,18 @@ __version__: str = ...
 T = TypeVar('T')
 U = TypeVar('U')
 
+
 class B(Generic[T, U]):
-    def __init__(self, f: Callable[[T], U]) -> None:
+    def __init__(self, f: Callable[[T], U], **kw: Any) -> None:
         ...
-        # self.f = f
 
 class Pipe(B[T, U]):
     def __ror__(self, x: T) -> U: ...
 
 
-def f(x: int) -> int: ...
-# def f(x) -> int: ...
+class Map(B[T, U]):
+    def __ror__(self, x: Iterable[T]) -> Iterator[U]: ...
+
+
+class Filter(B[T, U]):
+    def __ror__(self, x: Iterable[T]) -> Iterator[U]: ...
