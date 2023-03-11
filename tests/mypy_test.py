5f9561940b2635b4d2846934d5a200dc281fd0e4
Alexander Rodionov <tandav@tandav.me>
2023-03-11 20:52:57 +0400

    WIP

--- tests/mypy_test.py
+++ tests/mypy_test.py
@@ -1,5 +1,26 @@
 import pipe21 as P
 
+
+def f0(x: str) -> frozenset[int]:
+    return frozenset(x | P.Map(int))
+
+
+def f1(bits: str) -> frozenset[int]:
+    q = bits | P.Map(int)
+    reveal_type(q)
+    v = P.Pipe(frozenset)
+    reveal_type(v)
+    u = q | v
+    reveal_type(u)
+    return u
+
+    # return (
+    #     bits
+    #     | P.Map(int)
+    #     | P.Pipe(frozenset)
+    # )
+
+
 # def bits_to_intervals(bits: str) -> frozenset[int]:
 #     return (
 #         bits
@@ -9,11 +30,3 @@ import pipe21 as P
 #         | P.Keys()
 #         | P.Pipe(frozenset)
 #     )
-
-
-def bits_to_intervals(bits: str) -> frozenset[int]:
-    return (
-        bits
-        | P.Map(int)
-        | P.Pipe(frozenset)
-    )
