702c04ba9fcb575fae22893b968dcc8b784fe538
Alexander Rodionov <tandav@tandav.me>
2023-03-11 19:45:08 +0400

    WIP

--- typings/pipe21/__init__.pyi
+++ typings/pipe21/__init__.pyi
@@ -1,3 +1,6 @@
+__version__: str = ...
+
+
 from collections.abc import Callable
 from typing import Generic
 from typing import TypeVar
@@ -7,120 +10,11 @@ U = TypeVar('U')
 
 class Pipe(Generic[T, U]):
     def __init__(self, f: Callable[[T], U]) -> None:
-        self.f = f
+        ...
+        # self.f = f
 
     def __ror__(self, x: T) -> U: ...
 
-# def bits_to_intervals(bits: int) -> str:
-#     return bits | Pipe(str)
 
 def f(x: int) -> int: ...
-
-# from collections.abc import Generator
-# from collections.abc import Iterable
-# from collections.abc import Iterator
-# from collections.abc import Sequence
-# from pathlib import Path
-# from typing import Any
-# from typing import Callable
-# from typing import TypeVar
-
-# T = TypeVar('T')
-# K = TypeVar('K')
-# V = TypeVar('V')
-
-# todo: add f and kw annotations for every class
-
-# class B:
-#     f: Callable[..., Any]
-#     kw: Any
-#     def __init__(self, f: Callable[..., Any] | None = None, **kw: Any) -> None: ...
-
-# class Pipe(B):
-#     f: Callable[[T], V]
-#     def __ror__(self, x: T) -> V: ...
-
-# class Map(B):
-#     f: Callable[[K], V]
-#     def __ror__(self, x: Iterable[K]) -> Iterator[V]: ...
-
-# class Filter(B):
-#     def __ror__(self, x: Iterable[T]) -> Iterator[T]: ...
-
-# class Reduce(B):
-#     def __ror__(self, x: Iterable[T]) -> Any: ...
-
-# class MapValues(B):
-#     def __ror__(self, it: Iterable[tuple[K, V]]) -> Iterator[tuple[K, T]]: ...
-
-# class MapKeys(B):
-#     def __ror__(self, it: Iterable[tuple[K, V]]) -> Iterator[tuple[T, V]]: ...
-
-# class FilterFalse(B):
-#     def __ror__(self, it: Iterable[Any]) -> Iterator[Any]: ...
-
-# class FilterKeys(B):
-#     def __ror__(self, it: Iterable[tuple[K, V]]) -> Iterator[tuple[K, V]]: ...
-
-# class FilterValues(B):
-#     def __ror__(self, it: Iterable[tuple[K, V]]) -> Iterator[tuple[K, V]]: ...
-
-# class FlatMap(B):
-#     def __ror__(self, x: Iterable[T]) -> Iterator[V]: ...
-
-# class FlatMapValues(B):
-#     def __ror__(self, it: Iterable[tuple[K, V]]) -> Iterator[tuple[K, T]]: ...
-
-# class KeyBy(B):
-#     def __ror__(self, it: Iterable[V]) -> Iterator[tuple[K, V]]: ...
-
-# class ValueBy(B):
-#     def __ror__(self, it: Iterable[K]) -> Iterator[tuple[K, V]]: ...
-
-# class Append(B):
-#     def __ror__(self, it: Iterable[Iterable[Any]]) -> Iterable[Iterable[Any]]: ...
-
-# class Keys(B):
-#     def __ror__(self, it: Iterable[tuple[K, V]]) -> Iterator[K]: ...
-
-# class Values(B):
-#     def __ror__(self, it: Iterable[tuple[K, V]]) -> Iterator[V]: ...
-
-# class Grep(B):
-#     def __ror__(self, x: Iterable[T]) -> Iterator[T]: ...
-
-# class GrepV(B):
-#     def __ror__(self, x: Iterable[T]) -> Iterator[T]: ...
-
-# class Count(B):
-#     def __ror__(self, x: Iterable[T]) -> int: ...
-
-# class Take(B):
-#     def __ror__(self, x: Iterable[T]) -> tuple[T, ...]: ...
-
-# class Chunked(B):
-#     def __ror__(self, x: Iterable[T]) -> Iterable[tuple[T, ...]]: ...
-
-# class GroupBy(B):
-#     def __ror__(self, x: Iterable[T]) -> Iterator[tuple[T, Iterator[T]]]: ...
-
-# class ReadLines(B):
-#     def __ror__(self, fn: str | Path) -> list[str]: ...
-
-# class PipeArgs(B):
-#     def __ror__(self, x: Iterable[Any]) -> Any: ...
-
-# class StarMap(B):
-#     def __ror__(self, x: Iterable[Iterable[Any]]) -> Iterable[Any]: ...
-
-# class IsUnique(B):
-#     def __ror__(self, seq: Sequence[Any]) -> bool: ...
-
-# class Sorted(B):
-#     def __ror__(self, x: Iterable[T]) -> list[T]: ...
-
-# class Unique(B):
-#     def __ror__(self, it: Iterable[T]) -> Generator[T, None, None]: ...
-
-# class MapApply(B):
-#     def __ror__(self, it: Iterable[T]) -> Generator[T, None, None]: ...
+# def f(x) -> int: ...
