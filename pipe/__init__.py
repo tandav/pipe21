2f637543a5459226269f3110344566dbd2ee39a0
Alexander Rodionov <tandav@tandav.me>
2023-03-11 17:25:16 +0400

    WIP

--- pipe/__init__.py
+++ pipe/__init__.py
@@ -1,15 +1,8 @@
-from collections.abc import Callable
-from typing import Generic
-from typing import TypeVar
-
-T = TypeVar('T')
-U = TypeVar('U')
-
-class Pipe(Generic[T, U]):
-    def __init__(self, f: Callable[[T], U]) -> None:
+class Pipe:
+    def __init__(self, f):
         self.f = f
 
-    def __ror__(self, x: T) -> U:
+    def __ror__(self, x):
         return self.f(x)
 
 def bits_to_intervals(bits: int) -> str:
