86609877bebc976359022f6a678a914e16568ef5
Alexander Rodionov <tandav@tandav.me>
2023-03-12 16:48:40 +0400

    add implementation using __call__

--- pipe21.py
+++ pipe21.py
@@ -6,62 +6,76 @@ __version__ = '1.8.0'
 
 
 class B:
-    def __init__(self, f=None, *args, **kw):
-        self.f = f
-        self.args = args
-        self.kw = kw
+    def __init__(self, op):
+        self.op = op
+
+    def __call__(self, f=None, *args, **kw):
+        self.f = f  # pylint: disable=attribute-defined-outside-init
+        self.args = args  # pylint: disable=attribute-defined-outside-init
+        self.kw = kw  # pylint: disable=attribute-defined-outside-init
+        return self
+
+    def __ror__(self, other):
+        return self.op(self, other)
 
 
-class Pipe         (B): __ror__ = lambda self, x: self.f(x)
-class Map          (B): __ror__ = lambda self, it: map(self.f, it)
-class Filter       (B): __ror__ = lambda self, it: filter(self.f, it)
-class Reduce       (B): __ror__ = lambda self, it: functools.reduce(self.f, it, *self.args)
-class MapValues    (B): __ror__ = lambda self, it: it | Map(lambda kv: (kv[0], self.f(kv[1])))
-class MapKeys      (B): __ror__ = lambda self, it: it | Map(lambda kv: (self.f(kv[0]), kv[1]))
-class FilterFalse  (B): __ror__ = lambda self, it: it | Filter(lambda x: not self.f(x))
-class FilterKeys   (B): __ror__ = lambda self, it: it | Filter(lambda x: (self.f or bool)(x[0]))
-class FilterValues (B): __ror__ = lambda self, it: it | Filter(lambda x: (self.f or bool)(x[1]))
-class FlatMap      (B): __ror__ = lambda self, it: it | Map(self.f) | Pipe(itertools.chain.from_iterable)
-class FlatMapValues(B): __ror__ = lambda self, it: it | FlatMap(lambda kv: ((kv[0], x) for x in self.f(kv[1])))
-class KeyBy        (B): __ror__ = lambda self, it: it | Map(lambda x: (self.f(x), x))
-class ValueBy      (B): __ror__ = lambda self, it: it | Map(lambda x: (x, self.f(x)))
-class Append       (B): __ror__ = lambda self, it: it | Map(lambda x: (*x, self.f(x)))
-class Keys         (B): __ror__ = lambda self, it: it | Map(lambda x: x[0])
-class Values       (B): __ror__ = lambda self, it: it | Map(lambda x: x[1])
-class Grep         (B): __ror__ = lambda self, it: it | Filter(lambda x: re.search(self.f, x))
-class GrepV        (B): __ror__ = lambda self, it: it | Filter(lambda x: not re.search(self.f, x))
-class Count        (B): __ror__ = lambda self, it: sum(1 for _ in it)
-class Slice        (B): __ror__ = lambda self, it: itertools.islice(it, self.f, *self.args)
-class Take         (B): __ror__ = lambda self, it: it | Slice(self.f) | Pipe(tuple)
-class Chunked      (B): __ror__ = lambda self, it: iter(functools.partial(lambda n, i: i | Take(n), self.f, iter(it)), ())
-class GroupBy      (B): __ror__ = lambda self, it: itertools.groupby(it, key=self.f)
-class PipeArgs     (B): __ror__ = lambda self, x: self.f(*x)
-class StarMap      (B): __ror__ = lambda self, x: x | Map(lambda y: y | PipeArgs(self.f))
-class IsUnique     (B): __ror__ = lambda self, seq: len(seq) == len(set(seq if self.f is None else map(self.f, seq)))
-class Sorted       (B): __ror__ = lambda self, it: sorted(it, **self.kw)
-class MapApply     (B): __ror__ = lambda self, it: it | Map(lambda x: x | Apply(self.f))
-class ReduceByKey  (B): __ror__ = lambda self, it: it | Sorted(lambda kv: kv[0]) | GroupBy(lambda kv: kv[0]) | MapValues(lambda kv: kv | Values() | Reduce(self.f)) | Pipe(list)
+Pipe = B(lambda self, x: self.f(x))
+Map = B(lambda self, x: map(self.f, x))
+Filter = B(lambda self, x: filter(self.f, x))
+
+Reduce = B(lambda self, x: functools.reduce(self.f, x, *self.args))
+MapValues = B(lambda self, x: x | Map(lambda kv: (kv[0], self.f(kv[1]))))
+MapKeys = B(lambda self, x: x | Map(lambda kv: (self.f(kv[0]), kv[1])))
+FilterFalse = B(lambda self, x: x | Filter(lambda x: not self.f(x)))
+FilterKeys = B(lambda self, x: x | Filter(lambda x: (self.f or bool)(x[0])))
+FilterValues = B(lambda self, x: x | Filter(lambda x: (self.f or bool)(x[1])))
+FlatMap = B(lambda self, x: x | Map(self.f) | Pipe(itertools.chain.from_iterable))
+FlatMapValues = B(lambda self, x: x | FlatMap(lambda kv: ((kv[0], y) for y in self.f(kv[1]))))
+KeyBy = B(lambda self, x: x | Map(lambda x: (self.f(x), x)))
+ValueBy = B(lambda self, x: x | Map(lambda x: (x, self.f(x))))
+Append = B(lambda self, x: x | Map(lambda x: (*x, self.f(x))))
+Keys = B(lambda self, x: x | Map(lambda x: x[0]))
+Values = B(lambda self, x: x | Map(lambda x: x[1]))
+Grep = B(lambda self, x: x | Filter(lambda x: re.search(self.f, x)))
+GrepV = B(lambda self, x: x | Filter(lambda x: not re.search(self.f, x)))
+Count = B(lambda self, x: sum(1 for _ in x))
+Slice = B(lambda self, x: itertools.islice(x, self.f, *self.args))
+Take = B(lambda self, x: x | Slice(self.f) | Pipe(tuple))
+Chunked = B(lambda self, x: iter(functools.partial(lambda n, i: i | Take(n), self.f, iter(x)), ()))
+GroupBy = B(lambda self, x: itertools.groupby(x, key=self.f))
+PipeArgs = B(lambda self, x: self.f(*x))
+StarMap = B(lambda self, x: x | Map(lambda y: y | PipeArgs(self.f)))
+IsUnique = B(lambda self, x: len(set(x)) == len(x))
+Sorted = B(lambda self, x: sorted(x, **self.kw))
+MapApply = B(lambda self, x: x | Map(lambda y: y | Apply(self.f)))
+ReduceByKey = B(lambda self, x: x | Sorted(lambda kv: kv[0]) | GroupBy(lambda kv: kv[0]) | MapValues(lambda kv: kv | Values() | Reduce(self.f)) | Pipe(list))
 
 
-class Unique(B):
-    def __ror__(self, it):
-        key = self.f or (lambda x: x)
-        seen = set()
-        for item in it:
-            k = key(item)
-            if k in seen:
-                continue
-            seen.add(k)
-            yield item
+def unique(self, it):
+    key = self.f or (lambda x: x)
+    seen = set()
+    for item in it:
+        k = key(item)
+        if k in seen:
+            continue
+        seen.add(k)
+        yield item
 
 
-class Apply(B):
-    def __ror__(self, x):
-        self.f(x)
-        return x
+Unique = B(unique)
 
 
-class IterLines(B):
-    def __ror__(self, fn):
-        with open(fn) as f:
-            yield from f
+def apply(self, x):
+    self.f(x)
+    return x
+
+
+Apply = B(apply)
+
+
+def iter_lines(self, fn):  # pylint: disable=W0613
+    with open(fn) as f:
+        yield from f
+
+
+IterLines = B(iter_lines)
