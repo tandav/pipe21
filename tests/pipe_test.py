ad71fdee863c9b20482a4aed24f777b6f0066307
Alexander Rodionov <tandav@tandav.me>
2023-04-06 10:53:23 +0300

    stricter ruff rules (use ALL)

--- tests/pipe_test.py
+++ tests/pipe_test.py
@@ -35,7 +35,7 @@ def test_filter(it):
 
 
 @pytest.mark.parametrize(
-    'it, initializer', [
+    ('it', 'initializer'), [
         ([], 0),
         ([], None),
         ([1, 2, 3], 0),
@@ -76,40 +76,40 @@ def test_filter_false(it):
 
 
 @pytest.mark.parametrize(
-    'it, f ,expected', (
+    ('it', 'f', 'expected'), [
         ([(0, 2), (3, 0)], None, [(3, 0)]),
         ([(0, 2), (3, 0)], lambda x: x % 2 == 0, [(0, 2)]),
-    ),
+    ],
 )
 def test_filter_keys(it, f, expected):
     assert it | FilterKeys(f) | Pipe(list) == expected
 
 
 @pytest.mark.parametrize(
-    'it, f ,expected', (
+    ('it', 'f', 'expected'), [
         ([(0, 2), (3, 0)], None, [(0, 2)]),
         ([(0, 2), (3, 0)], lambda x: x % 2 == 0, [(0, 2), (3, 0)]),
-    ),
+    ],
 )
 def test_filter_values(it, f, expected):
     assert it | FilterValues(f) | Pipe(list) == expected
 
 
 @pytest.mark.parametrize(
-    'it, f ,expected', (
+    ('it', 'f', 'expected'), [
         ([0, 2, 3, 0, 4], range, [0, 1, 0, 1, 2, 0, 1, 2, 3]),
         ([2, 3, 4], lambda x: [(x, x), (x, x)], [(2, 2), (2, 2), (3, 3), (3, 3), (4, 4), (4, 4)]),
         ([range(0, 5), range(100, 105)], yield_even, [0, 2, 4, 100, 102, 104]),
-    ),
+    ],
 )
 def test_flat_map(it, f, expected):
     assert it | FlatMap(f) | Pipe(list) == expected
 
 
 @pytest.mark.parametrize(
-    'it, f ,expected', (
+    ('it', 'f', 'expected'), [
         ([('a', ['x', 'y', 'z']), ('b', ['p', 'r'])], lambda x: x, [('a', 'x'), ('a', 'y'), ('a', 'z'), ('b', 'p'), ('b', 'r')]),
-    ),
+    ],
 )
 def test_flat_map_values(it, f, expected):
     assert it | FlatMapValues(f) | Pipe(list) == expected
@@ -121,10 +121,10 @@ def test_key_by_value_by():
 
 
 @pytest.mark.parametrize(
-    'it, f ,expected', (
+    ('it', 'f', 'expected'), [
         ([(0,), (1,)], lambda x: str(x[0]), [(0, '0'), (1, '1')]),
         ([(0, '0'), (1, '1')], lambda x: str(x[0] * 10), [(0, '0', '0'), (1, '1', '10')]),
-    ),
+    ],
 )
 def test_append(it, f, expected):
     assert it | Append(f) | Pipe(list) == expected
@@ -132,7 +132,7 @@ def test_append(it, f, expected):
 
 
 @pytest.mark.parametrize(
-    'it, expected', [
+    ('it', 'expected'), [
         ([(0, 'a'), (1, 'b')], [0, 1]),
     ],
 )
@@ -141,7 +141,7 @@ def test_keys(it, expected):
 
 
 @pytest.mark.parametrize(
-    'it, expected', [
+    ('it', 'expected'), [
         ([(0, 'a'), (1, 'b')], ['a', 'b']),
     ],
 )
@@ -150,7 +150,7 @@ def test_values(it, expected):
 
 
 @pytest.mark.parametrize(
-    'it, grep, expected', [
+    ('it', 'grep', 'expected'), [
         (['hello 42 bro', 'world', 'awesome 42'], '42', ['hello 42 bro', 'awesome 42']),
         (['foo1', 'foo2', '3foo', 'bar1'], '^foo.*', ['foo1', 'foo2']),
     ],
@@ -160,7 +160,7 @@ def test_grep(it, grep, expected):
 
 
 @pytest.mark.parametrize(
-    'it, grep, expected', [
+    ('it', 'grep', 'expected'), [
         (['hello 42 bro', 'world', 'awesome 42'], '42', ['world']),
         (['foo1', 'foo2', '3foo', 'bar1'], '^foo.*', ['3foo', 'bar1']),
     ],
@@ -170,58 +170,58 @@ def test_grep_v(it, grep, expected):
 
 
 @pytest.mark.parametrize(
-    'it, expected', (
+    ('it', 'expected'), [
         (range(3), 3),
         (iter(range(3)), 3),
         ('abc', 3),
         ({1, 2, 3}, 3),
         ({'a': 1, 'b': 2}, 2),
-    ),
+    ],
 )
 def test_count(it, expected):
     assert it | Count() == expected
 
 
 @pytest.mark.parametrize(
-    'it, args, expected', (
+    ('it', 'args', 'expected'), [
         (range(5), (2,), [0, 1]),
         (range(5), (2, 4), [2, 3]),
         (range(5), (2, None), [2, 3, 4]),
         (range(5), (0, None, 2), [0, 2, 4]),
-    ),
+    ],
 )
 def test_slice(it, args, expected):
     assert it | Slice(*args) | Pipe(list) == expected
 
 
 @pytest.mark.parametrize(
-    'it, n, expected', (
+    ('it', 'n', 'expected'), [
         (range(5), 3, (0, 1, 2)),
         (range(5), 1, (0,)),
         (range(5), 0, ()),
         (range(5), 10, (0, 1, 2, 3, 4)),
-    ),
+    ],
 )
 def test_take(it, n, expected):
     assert it | Take(n) == expected
 
 
 @pytest.mark.parametrize(
-    'it, n, expected', (
+    ('it', 'n', 'expected'), [
         (range(5), 5, [(0, 1, 2, 3, 4)]),
         (range(5), 4, [(0, 1, 2, 3), (4,)]),
         (range(5), 3, [(0, 1, 2), (3, 4)]),
         (range(5), 2, [(0, 1), (2, 3), (4,)]),
         (range(5), 1, [(0,), (1,), (2,), (3,), (4,)]),
         (range(5), 0, []),
-    ),
+    ],
 )
 def test_chunked(it, n, expected):
     assert it | Chunked(n) | Pipe(list) == expected
 
 
 @pytest.mark.parametrize(
-    'it, f, expected', [
+    ('it', 'f', 'expected'), [
         ([(0, 'a'), (0, 'b'), (1, 'c'), (2, 'd')], operator.itemgetter(0), [(0, [(0, 'a'), (0, 'b')]), (1, [(1, 'c')]), (2, [(2, 'd')])]),
         (['ab', 'cd', 'e', 'f', 'gh', 'ij'], len, [(2, ['ab', 'cd']), (1, ['e', 'f']), (2, ['gh', 'ij'])]),
     ],
@@ -231,7 +231,7 @@ def test_groupby(it, f, expected):
 
 
 @pytest.mark.parametrize(
-    'it, f, expected', [
+    ('it', 'f', 'expected'), [
         ((1, 2), operator.add, 3),
         (('FF', 16), int, 255),
         (([1, 2], 'A'), dict.fromkeys, {1: 'A', 2: 'A'}),
@@ -243,7 +243,7 @@ def test_pipe_args(it, f, expected):
 
 
 @pytest.mark.parametrize(
-    'it, f, expected', [
+    ('it', 'f', 'expected'), [
         ([(2, 5), (3, 2), (10, 3)], pow, [32, 9, 1000]),
         ([('00', 16), ('A5', 16), ('FF', 16)], int, [0, 165, 255]),
     ],
@@ -253,26 +253,26 @@ def test_star_map(it, f, expected):
 
 
 @pytest.mark.parametrize(
-    'seq, key, expected', (
+    ('seq', 'key', 'expected'), [
         ([0, 1, 2, 3], None, True),
         ([0, 1, 1, 3], None, False),
         ('0123', int, True),
         ('0113', int, False),
-    ),
+    ],
 )
 def test_is_unique(seq, key, expected):
     assert seq | IsUnique(key) == expected
 
 
 @pytest.mark.parametrize(
-    'it, kw', (
+    ('it', 'kw'), [
         ([3, 5, 1, 0], {}),
         ([3, 5, 1, 0], {'reverse': True}),
         ('3510', {'key': int}),
         ('3510', {'key': int, 'reverse': True}),
         ('9j8xy2m#98g%^xd$', {'key': ord, 'reverse': True}),
         ('9j8xy2m#98g%^xd$', {'key': ord, 'reverse': False}),
-    ),
+    ],
 )
 def test_sorted(it, kw):
     assert it | Sorted(**kw) == sorted(it, **kw)
@@ -284,7 +284,7 @@ def test_map_apply():
 
 
 @pytest.mark.parametrize(
-    'it, f, expected', [
+    ('it', 'f', 'expected'), [
         ([('a', 1), ('b', 1), ('a', 1)], operator.add, [('a', 2), ('b', 1)]),
     ],
 )
@@ -293,13 +293,13 @@ def test_reduce_by_key(it, f, expected):
 
 
 @pytest.mark.parametrize(
-    'seq, key, expected', (
+    ('seq', 'key', 'expected'), [
         ([0, 1, 1, 2], None, [0, 1, 2]),
         ('0112', int, ['0', '1', '2']),
         (range(10), lambda x: x % 3, [0, 1, 2]),
         (['a', 'cd', 'cd', 'e', 'fgh'], None, ['a', 'cd', 'e', 'fgh']),
         (['a', 'cd', 'cd', 'e', 'fgh'], len, ['a', 'cd', 'fgh']),
-    ),
+    ],
 )
 def test_unique(seq, key, expected):
     assert seq | Unique(key) | Pipe(list) == expected
